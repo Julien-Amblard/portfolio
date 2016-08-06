ecc560a515f36240095ea0e1a89f5fb5fbcf26d3
Julien-Amblard <amblard.julien@gmail.com>
2016-08-06 18:56:16 +0200

    move js files

--- ui/build/js/helpers.js
+++ ui/build/js/helpers.js
@@ -1,8 +1,14 @@
-let rand = ( min, max ) => { return Math.random() * ( max - min) + min; };
-let globalResize =  () => { oSize.w = canvas.width = window.innerWidth;  oSize.h = canvas.height = window.innerHeight; };
-let oSize 	= {
-	h : window.innerHeight,
-	w : window.innerWidth
+'use strict';
+
+var rand = function rand(min, max) {
+	return Math.random() * (max - min) + min;
+};
+var globalResize = function globalResize() {
+	oSize.w = canvas.width = window.innerWidth;oSize.h = canvas.height = window.innerHeight;
+};
+var oSize = {
+	h: window.innerHeight,
+	w: window.innerWidth
 };
 
-window.addEventListener( 'resize', globalResize, false);
+window.addEventListener('resize', globalResize, false);
